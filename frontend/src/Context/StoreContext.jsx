20ebcfe0265a8090fcc0988cd0d3ec29e995299a
arshu200 <shaik22arshad@gmial.com>
2025-01-20 15:33:12 +0530

    first commit in new branch

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = import.meta.env.BACKEND_URL ||"http://localhost:4000"
+    const url = "http://backend.backend.svc.cluster.local"
     const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("")
